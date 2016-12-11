c7a3aaf2c8214172c02c2756f1e18e8648932821
sebastien delneste <seb@macmini.lan>
2016-12-11 10:36:47 +0100

    non-recursive day9

--- day09.py
+++ day09.py
@@ -8,15 +8,20 @@ def expand_pattern(p):
   if g:
     (prefix, char, multiple, rest) = g.groups()
     n = rest[:int(char)] * int(multiple)
-    return prefix + expand_pattern(n) + expand_pattern(rest[int(char):])
+    return prefix + n + rest[int(char):]
   else:
+    print "Error: %s" % p
+    exit()
 #    print len(p)
     return p
 
 def part1(data):
   """data is an array; important for assignment by reference."""
-  data[0].replace(' ', '')
-  print len(expand_pattern(data[0]))
+  data[0] = data[0].replace(' ', '')
+  while '(' in data[0]:
+    data[0] = expand_pattern(data[0])
+    print 'Len: %08d Count "(": %04d Count ")": %04d' % (len(data[0]), data[0].count('('), data[0].count(')'))
+  print 'LEN: %d' % len(data[0])
 
 data = open('data/day09.txt', 'r').readlines()
 part1(data)
